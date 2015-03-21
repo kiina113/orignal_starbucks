e21fe1b9d21fc2246e991e6a0a42818a0159fba1
KINARI NISHIYAMA <KINARI@nishiyamaseisei-no-MacBook-Air.local>
2015-03-22 08:11:30 +0900

    first commit

---
+++ starbucksapp/CoffeeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CoffeeViewController.swift
+//  starbucksapp
+//
+//  Created by KINARI NISHIYAMA on 2015/03/22.
+//  Copyright (c) 2015年 KINARI NISHIYAMA. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
